c282b37fd54f35c668448257939b0084ea3595da
= <jwparker284@gmail.com>
2023-09-11 18:12:33 -0700

    updated the categories layout to be more user friendly

--- src/components/Products/CategoriesContainer.jsx
+++ src/components/Products/CategoriesContainer.jsx
@@ -2,7 +2,7 @@ import ProductsCard from "./ProductsCard"
 
 export default function CategoriesContainer({products}) {
     console.log(`$Container: ${products}`)
-    return (<div className=" flex flex-row  w-full flex items-center ">
+    return (<div className=" flex flex-row  w-full flex items-center overflow-x-scroll ">
         {products.map((product) => {
             console.log("PRODUCT: " + product)
             return <ProductsCard key={product.id} product={product}></ProductsCard>
