cd4e9c7bfa927d5d557d7576b02658b87a4bec6b
= <jwparker284@gmail.com>
2023-09-21 07:25:41 -0700

    implemented filters for products

--- src/components/Products/FilterView.jsx
+++ src/components/Products/FilterView.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
+import { sortPriceHighToLow, sortPriceLessThan100, sortPriceLowToHigh } from "../../utils/productFilters";
 
-export default function FilterView() {
+export default function FilterView({products, setProductCategories}) {
     const [dropDownToggle, setDropDownToggle] = useState(false);
     return (
 
@@ -19,17 +20,28 @@ export default function FilterView() {
         <div className="py-1 " role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             <a href="#" className="text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
                 <span className="flex flex-col">
-                <button className="mb-2 w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg px-3 py-2 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>Low to high</button>
+                <button onClick={async() => {
+                    const filteredProducts = await sortPriceLowToHigh(products);
+                    setProductCategories(filteredProducts);
+                }} className="mb-2 w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg 
+                px-3 py-1 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>Low to high</button>
                 </span>
             </a>
             <a href="#" className="text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
                 <span className="flex flex-col">
-                <button className="mb-2 w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg px-3 py-2 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>High to low</button>
+                <button onClick={async()=> {
+                    const filteredProducts = await sortPriceHighToLow(products);
+                    setProductCategories(filteredProducts);
+
+                }} className="mb-2 w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg px-3 py-1 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>High to low</button>
                 </span>
             </a>
             <a href="#" className=" text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
                 <span className="flex flex-col">
-                <button className="block w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg px-3 py-2 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>Less than $100</button>
+                <button onClick={async() => {
+                    const filteredProducts = await sortPriceLessThan100(products);
+                    setProductCategories(filteredProducts);
+                }} className="block w-full max-w-xs mx-auto bg-blue-600 hover:bg-blue-500 transition duration-300 text-white rounded-lg px-3  font-semibold"><i className="mdi mdi-lock-outline mr-1"></i>Less than $100</button>
                 </span>
             </a>
         </div>
