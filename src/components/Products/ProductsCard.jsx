374d1a361eff7093ab1975efb00780dca8231761
= <jwparker284@gmail.com>
2023-09-25 05:25:34 -0700

    updated UI to work when product is null

--- src/components/Products/ProductsCard.jsx
+++ src/components/Products/ProductsCard.jsx
@@ -11,7 +11,7 @@ export default function ProductsCard({product, username}) {
     return (
         <div className="h-90vh flex justify-start w-auto mb-4 mt-4">
             <div className="flex items-center h-400 mr-4 ml-4 w-80 md:w-80 sm:w-60 xs:w-60">
-                <div className="card flex flex-col justify-center w-full p-20 bg-white rounded-lg shadow-2xl">
+                {product && <div className="card flex flex-col justify-center w-full p-20 bg-white rounded-lg shadow-2xl">
                 <div className="prod-title">
                     <p className="text-sm uppercase text-gray-900 font-bold truncate">{product.title}</p>
                     <p className="uppercase text-sm text-gray-400">
@@ -27,17 +27,17 @@ export default function ProductsCard({product, username}) {
                     </div>
                     <div className="flex flex-col md:flex-row justify-between items-center text-gray-900">
                     
-                    <button onClick={() => {
+                    {username && <button onClick={() => {
                         addItemToCart(username,product.id, 1)
                         console.log(username)
                         
                     }}
                             className="px-12 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">Add
-                        to cart</button>
+                        to cart</button>}
                     </div>
                     <p className="font-bold text-xl">${product.price}</p>
                 </div>
-                </div>
+                </div>}
             </div>
         </div>
 
