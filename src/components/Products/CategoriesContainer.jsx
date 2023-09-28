1abad02cb82127722e575ec3850e21236cee084d
= <jwparker284@gmail.com>
2023-09-28 01:30:10 -0700

    fixed tailwind scroll x bug

--- src/components/Products/CategoriesContainer.jsx
+++ src/components/Products/CategoriesContainer.jsx
@@ -2,7 +2,7 @@ import ProductsCard from "./ProductsCard"
 
 export default function CategoriesContainer({products, username, setAddedToCartClicked}) {
     console.log(`$Container: ${products}`)
-    return (<div className=" flex justify-start w-full overflow-x-scroll ">
+    return (<div className="flex flex-none justify-start w-full overflow-x-scroll ">
         {products.map((product) => {
             console.log("PRODUCT: " + product)
             return <ProductsCard key={product.id} product={product} username={username} setAddedToCartClicked={setAddedToCartClicked}></ProductsCard>
